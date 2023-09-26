d593a2e9fe386f92075629d22005a3b8f3a8c862
Mary Angelene V. Balais <95016909+TheMoreTheMary-er@users.noreply.github.com>
2023-09-27 06:51:03 +0800

    Changed the Layout of the Website
    Added a responsive top navigation menu, added a Hero section, Re-located the image and title of the "About Me" section and added a functional button where it will direct to another link. Changed the "My Works" project layouts, but kept the hovering effect. I just added a picture at the "Contact" section to maximize the space.

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-//Animation of Skills, Experiences, Education in About Me Section
-var tablinks = document.getElementsByClassName("tab-links");
-    var tabcontents = document.getElementsByClassName("tab-contents");
-    function opentab(event,tabname){
-        for(var tablink of tablinks){
-            tablink.classList.remove("active-link");
-        }
-        for(var tabcontent of tabcontents){
-            tabcontent.classList.remove("active-tab");
-        }
-        event.currentTarget.classList.add("active-link");
-        document.getElementById(tabname).classList.add("active-tab")
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const menu = document.querySelector(".menu-list");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick= ()=>{
+    menu.classList.add("active");
+    menuBtn.classList.add("hide");
+    body.classList.add("disabledScroll");
     }
-
+cancelBtn.onclick= ()=>{
+    menu.classList.remove("active");
+    menuBtn.classList.remove("hide");
+    body.classList.remove("disabledScroll");
+    }
+window.onscroll= ()=>{
+    this.scrollY > 20 ? navbar.classList.add("sticky"): navbar.classList.remove("sticky");
+    }
